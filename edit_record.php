61181ce2908b916975da5b82aec24a5f638e7327
naiyuki <ianbinuya1@gmail.com>
2025-04-28 13:33:03 +0800

    edit goods

--- edit_record.php
+++ edit_record.php
@@ -1,15 +1,6 @@
 <?php
-// Database connection
-$servername = "tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "vl9ieik1ttwerlmd"; // Your DB username
-$password = "dxn55zzkhyp5ek1e";     // Your DB password
-$dbname = "z6vet51amyrj9ci0"; // Your DB name
+include 'dbconnection.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // Get the 'id' from the URL
 if (isset($_GET['id']) && !empty($_GET['id'])) {
--- update_record.php
+++ update_record.php
@@ -1,16 +1,5 @@
 <?php
-// Database connection
-$servername = "tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "vl9ieik1ttwerlmd"; // Your DB username
-$password = "dxn55zzkhyp5ek1e";     // Your DB password
-$dbname = "z6vet51amyrj9ci0"; // Your DB name
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'dbconnection.php';
 
 // Check if id and other details are posted
 if (isset($_POST['id']) && isset($_POST['Student_Name']) && isset($_POST['Department']) && isset($_POST['Program']) && isset($_POST['Violation']) && isset($_POST['Offense']) && isset($_POST['Status']) && isset($_POST['Date'])) {
