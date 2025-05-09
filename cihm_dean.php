7a216187e36316117ac410f993c34a313d7af07e
naiyuki <ianbinuya1@gmail.com>
2025-05-09 21:02:41 +0800

    inayos ko lang yung title ng deans

--- cihm_dean.php
+++ cihm_dean.php
@@ -7,7 +7,7 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link rel="icon" type="image/png" sizes="32x32" href="img/plp.png">
-    <title>CIHM Dean</title>
+    <title>VMS. (CIHM)</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js"></script>
 
